e6f617e676c5121949419cd82d3be1ef2cc4570e
= <=>
2018-07-14 16:33:03 +0200

    fds

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -8,11 +8,11 @@
 @foreach($questions as $question)
 
     <div class="row">
-<div class="col-md-5"><a style="color:black" href="/questions/show/{{$question->id}}">{{$question->question}}</a></div>
+<div class="col-md-5"><a style="color:black" href="{{route('show', $question->id)}}">{{$question->question}}</a></div>
 <div class="col-md-1">{{$question->channel}}</div>
 <div class="col-md-1">{{$question->id}}</div>
 
-<div class="col-md-2"><a href="/edit/{{$question->id}}">Edytuj</a></div>
+<div class="col-md-2"><a href="{{route('edit', $question->id)}}">Edytuj</a></div>
 </div>
 @endforeach
 @endsection
