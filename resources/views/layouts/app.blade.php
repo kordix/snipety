f2def170f07b32c302649c0ba525b618dcecfe5b
= <=>
2018-12-30 13:31:42 +0100

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>SnipetAppka</title>
     <link rel="stylesheet" href="{{asset('css/prism.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
@@ -50,6 +50,7 @@
                             @endforeach
                         </ul>
                     </ul>
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-5">Pytanie</div><div class="col-md-1">Channel</div><div class="col-md-1">Id</div><div class="col-md-2"></div>
+        <div class="col-md-5">Pytanie</div><div class="col-md-1">Channel</div><div class="col-md-1">Id</div><div class="col-md-1">Counter</div><div class="col-md-2"></div>
     </div>
 @foreach($questions as $question)
 
@@ -11,6 +11,7 @@
 <div class="col-md-5"><a style="color:black" href="{{route('show', $question->id)}}">{{$question->question}}</a></div>
 <div class="col-md-1">{{App\Channel::find($question->channel)['channelname']}}</div>
 <div class="col-md-1">{{$question->id}}</div>
+<div class="col-md-1">{{$question->counter}}</div>
 
 <div class="col-md-2"><a href="{{route('edit', $question->id)}}">Edytuj</a></div>
 </div>
